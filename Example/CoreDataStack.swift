81b0e8f6b8d190fc3c4996d51ba32d06693616a9
Julien Henrard <j.henrard@nexmind.com>
2016-04-24 17:39:12 +0200

    Add CoreDataStack as example

---
+++ Example/CoreDataStack.swift
@@ -0,0 +1,69 @@
+//
+//  CoreDataStack.swift
+//  Swiftizy
+//
+//  Reference from the book: "CoreData by tutorials second edition"
+//  by Aaron Douglas, Saul Mora, Matthew Morey and Pietro Rea
+
+import CoreData
+
+class CoreDataStack {
+    
+    let modelName = "yourModelName"
+    
+    lazy var context: NSManagedObjectContext = {
+        
+        var managedObjectContext = NSManagedObjectContext(
+            concurrencyType: .MainQueueConcurrencyType)
+        
+        managedObjectContext.persistentStoreCoordinator = self.psc
+        return managedObjectContext
+    }()
+    
+    private lazy var psc: NSPersistentStoreCoordinator = {
+        
+        let coordinator = NSPersistentStoreCoordinator(
+            managedObjectModel: self.managedObjectModel)
+        
+        let url = self.applicationDocumentsDirectory
+            .URLByAppendingPathComponent(self.modelName)
+        
+        do {
+            let options =
+            [NSMigratePersistentStoresAutomaticallyOption : true]
+            
+            try coordinator.addPersistentStoreWithType(
+                NSSQLiteStoreType, configuration: nil, URL: url,
+                options: options)
+        } catch  {
+            print("Error adding persistent store.")
+        }
+        
+        return coordinator
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        
+        let modelURL = NSBundle.mainBundle()
+            .URLForResource(self.modelName,
+                withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    private lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(
+            .DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    func saveContext () {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print("Error: \(error.localizedDescription)")
+                abort()
+            }
+        }
+    }
+}
