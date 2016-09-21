4a0f0d3d6d6e967c3e62a8221c27a29590842532
Julien Henrard <j.henrard@nexmind.com>
2016-09-21 13:31:47 +0200

    Add anyclass for creating entity

--- Swiftizy.podspec
+++ Swiftizy.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "Swiftizy"
-  s.version          = "1.3.2"
+  s.version          = "1.3.3"
   s.summary          = "Swiftizy help you in your development with CoreData, Rest, Json,..."
 
 # This description is used to generate tags and improve search results.
--- Swiftizy/Classes/Manager/Creating.swift
+++ Swiftizy/Classes/Manager/Creating.swift
@@ -22,10 +22,10 @@ public class Creating {
      - returns: NSManagerObject
      - parameter entityName:     The name of the entity to create
      */
-    public func entity(entity: String) -> NSManagedObject{
+    public func entity(entity: AnyClass) -> NSManagedObject{
         
         // On créée une entitée de type "Book" (un NSManagedObject). On récupere en premier, le type d'entitée, puis on créée un objet correspondant à cette entitée, et on l'insert dnas le managedContext
-        let entity = NSEntityDescription.entity(forEntityName: entity, in: managedContext)
+        let entity = NSEntityDescription.entity(forEntityName: NSStringFromClass(entity).pathExtension, in: managedContext)
         let object = NSManagedObject(entity: entity!, insertInto: managedContext)
         return object
         
@@ -36,7 +36,7 @@ public class Creating {
     public func exceptIfStringMatch(entity: AnyClass, attributeName: String, attributeValue: String) -> NSManagedObject? {
         let results = CoreDataManager.Fetch.equalString(entity: entity, attributeName: attributeName, attributeValue: attributeValue)
         if results.count == 0 {
-            return self.entity(entity: NSStringFromClass(entity).pathExtension)
+            return self.entity(entity: entity)
         }
         return nil
     }
@@ -44,7 +44,7 @@ public class Creating {
     public func exceptIfIntMatch(entity: AnyClass, attributeName: String, attributeValue: Int) -> NSManagedObject? {
         let results = CoreDataManager.Fetch.equalInt(entity: entity, attributeName: attributeName, attributeValue: attributeValue)
         if results.count == 0 {
-            return self.entity(entity: NSStringFromClass(entity).pathExtension)
+            return self.entity(entity: entity)
         }
         return nil
     }
